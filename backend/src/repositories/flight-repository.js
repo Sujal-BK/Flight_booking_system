60f2bdc0da7bc01b59760f5b78c16cbe09b41320
sujal4523 <sujalkachdiya@gmail.com>
2025-04-01 14:27:24 +0530

    added seat model

--- backend/src/repositories/flight-repository.js
+++ backend/src/repositories/flight-repository.js
@@ -1,16 +1,51 @@
 const CrudRepository = require('./CRUD-repository')
-const {Flight} = require('../models')
+const { Flight, Airplane, Airport, City } = require('../models')
+const { Sequelize } = require('sequelize')
 
-class FlightRepository extends CrudRepository{
-    constructor(){
+class FlightRepository extends CrudRepository {
+    constructor() {
         super(Flight)
 
     }
 
-    async getAllFlights(filter,sort){
-        const response =  await Flight.findAll({
-            where : filter,
-            order : sort
+    async getAllFlights(filter, sort) {
+        const response = await Flight.findAll({
+            where: filter,
+            order: sort,
+            include: [
+                {
+
+                    model: Airplane,
+                    required: true,
+                    as: 'airplaneDetail'
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'departureAirport',
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
+                    },
+                    include: {
+                        model: City,
+                        required: true
+                    }
+
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'arrivalAirport',
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+                    },
+                    include: {
+                        model: City,
+                        required: true
+                    }
+
+                }
+            ]
         })
         return response;
     }
