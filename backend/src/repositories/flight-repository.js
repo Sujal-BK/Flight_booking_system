8ce5e652dda8210f294bd73f79aa737e7bc83f90
sujal4523 <sujalkachdiya@gmail.com>
2025-04-05 20:08:59 +0530

    added flight api

--- backend/src/repositories/flight-repository.js
+++ backend/src/repositories/flight-repository.js
@@ -1,6 +1,8 @@
 const CrudRepository = require('./CRUD-repository')
 const { Flight, Airplane, Airport, City } = require('../models')
 const { Sequelize } = require('sequelize')
+const db = require('../models')
+const {addRowLockOnFlights} = require('./queries')
 
 class FlightRepository extends CrudRepository {
     constructor() {
@@ -52,6 +54,7 @@ class FlightRepository extends CrudRepository {
 
 
     async updateRemainingSeats(flightId,seats,dec=true){
+        await db.sequelize.query(addRowLockOnFlights(flightId))
         const flight = await Flight.findByPk(flightId)
         if(parseInt(dec)){
              await flight.decrement('totalSeats',{by:seats});
